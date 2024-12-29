ba928e4d31c66c8aacb0b173270c32df7ac4d033
hamza.bakaran <hamzabakaran@gmail.com>
2024-12-29 11:11:06 +0100

    Created repositories

---
+++ src/main/java/ba/edu/ibu/inventorymanagementwebapp/core/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package ba.edu.ibu.inventorymanagementwebapp.core.repository;
+
+import ba.edu.ibu.inventorymanagementwebapp.core.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+
+    List<Product> findByQuantityLessThan(int threshold);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+}
